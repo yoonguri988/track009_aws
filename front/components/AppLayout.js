1fbecc11957d0fc503306cf7e445e0101715ccd1
yoonguri988 <alice951816@gmail.com>
2026-09-03 10:45:38 +0900

    fix: 로그아웃 오류 수정

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -22,7 +22,17 @@ function AppLayout({  children , initialUser  }){   //★ 대체부품, 초기�
     const dispatch     = useDispatch();
     const {user}       = useSelector((state)=> state.auth); 
     
-    const handleLogout = ()=>{   dispatch(logoutRequest());   router.replace('/login');  };  // 디스패치(logoutRequest()) / 경로 login 넘기기   //##
+    const handleLogout = (e)=>{  
+        if(e && e.preventDefault) e.preventDefault();
+        dispatch(logoutRequest()); 
+        // router.replace('/login'); 
+        if(typeof window !== 'undefined'){
+            localStorage.removeItem('accessToken');
+            sessionStorage.clear();
+            window.location.href='/login';
+        }
+    };  // 디스패치(logoutRequest()) / 경로 login 넘기기   //##
+
 
     const menuItems = [
        ...( user  &&  user.nickname
